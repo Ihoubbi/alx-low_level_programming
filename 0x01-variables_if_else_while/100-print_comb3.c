2a3877889097f66091b273309527ce35ade107e3
ihoubbi <ihoubbi@gmail.com>
2023-06-16 17:27:29 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,16 +10,19 @@ int main(void)
 {
 	int n, m;
 
-	for (n = 48; n < 57; n++)
+	for (n = 48; n < 56; n++)
 	{
-		for (m = 49; m < 58; m++)
+		for (m = 49; m < 57; m++)
 		{
+			if (n < m)
+			{	
 			putchar(n);
 			putchar(m);
-			if (n != 57 || m != 58)
+			if (n != 56 || m != 57)
 			{
 				putchar(',');
 				putchar(' ');
+			}
 			}
 		}
 	}
