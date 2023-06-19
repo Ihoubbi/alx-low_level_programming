ba56a5528a05331814e1b5ded3e911161ba0a000
ihoubbi <ihoubbi@gmail.com>
2023-06-19 06:01:51 -0700

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -20,7 +20,7 @@ int main(void)
 				putchar(' ');
 				putchar((m / 10) + 48);
 				putchar((m % 10) + 48);
-				if (m != 98 || j != 99)
+				if (n != 98 || m != 99)
 				{
 					putchar(',');
 					putchar(' ');
