9a5b68e52166456b0108d0ef5c404ca3f6449684
ihoubbi <ihoubbi@gmail.com>
2023-06-20 17:29:55 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - dds two integers and returns the result.
+ * @i: int to be added to j
+ * @j: int to be added to i
+ * Return: sun of i and j
+ */
+
+int add(int i, int j)
+{
+	int k;
+
+	k = i + j;
+
+	rturn (k);
+}
