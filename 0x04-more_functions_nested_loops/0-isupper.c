fc2eb2d0be60f563b4dc232681618a90189ee35d
ihoubbi <ihoubbi@gmail.com>
2023-06-22 16:35:18 -0700

    checks for uppercase character.

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * _isupper - check for lowerse char
+ * @c: the cher to be checkrd
+ * Return: 1 for uppercase or 0 for anything else
+ */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
