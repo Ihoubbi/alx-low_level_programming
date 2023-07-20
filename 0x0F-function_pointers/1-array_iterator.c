a03bc633423550ba5b8a531423ffed847575e97f
ihoubbi <ihoubbi@gmail.com>
2023-07-20 10:39:55 -0700

    function given as a parameter on each element of an array.

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -12,7 +12,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t x;
 
-	if (array != null && action != null && size != null )
+	if (array && action && size)
 	{
 		for (x = 0; x < size; x++)
 			action(array[x]);
